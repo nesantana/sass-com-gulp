d146a6c84800ec3d7d794484eab29197e92cb42d
Mateus Santana <mateus.santana@before.com.br>
2019-10-18 17:47:24 -0400

    Gulp + Scss | Simples inicio de projeto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Novo projeto - Mateus Santana</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
+<link href="assets/css/default.min.css" rel="stylesheet" type="text/css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/scripts.min.js"></script>
+</head>
+<body>
+	<?php
+		include(header.php);
+	?>
+	<div class="inicio">
+		<h1>Novo projeto em scss</h1>
+	</div>
+	<?php
+		include(footer.php);
+	?>
+</body>
+
+</html>
